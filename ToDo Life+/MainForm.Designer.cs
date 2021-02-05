0b0a92c1662f5806ddf8b3a365f2fbd433bda504
sakaguchi_sho <dyfdv24@gmail.com>
2021-02-05 22:40:55 +0900

    フォーム名変更

--- ToDo Life+/MainForm.Designer.cs
+++ ToDo Life+/MainForm.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace ToDo_Life_
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// 必要なデザイナー変数です。
@@ -29,10 +29,17 @@ namespace ToDo_Life_
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "MainForm";
+            this.Text = "ToDo Life+";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
